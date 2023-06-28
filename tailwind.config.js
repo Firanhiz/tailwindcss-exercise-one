8026ef020212099c4b93e9a199bf75660d07b452
Firangiz <firanhizmamiedova@gmail.com>
2023-06-28 20:06:09 +0400

    added new fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true
+    },
+
+    extend: {
+      fontFamily: {
+        gemunu: ['Gemunu Libre', 'sans-serif'],
+        open: ['Open Sans', 'sans-serif']
+      },
+
+      colors: {
+        'my-green': '#AFD83A',
+        'my-aqua': '#9FE2BF',
+        'my-lilac': '#E9B7EE'
+      }
+    },
   },
   plugins: [],
 }
